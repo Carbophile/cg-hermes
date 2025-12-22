79c2b2f0a3fc91bd6046cc565b8411e344631d23
Borna Punda <borna.punda@carbophile.org>
2025-12-22 21:29:19 +0100

    feat(images): make images work in dev and preview envs (#60)
    Image optimization (and potentially other things in the future) is now controlled by the `HERMES_PRODUCTION` build-time env variable.

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -18,6 +18,10 @@ export function getCloudflareImage(
 	src: string,
 	{ width, quality }: { width?: number; quality?: number } = {},
 ) {
+	// biome-ignore lint/complexity/useLiteralKeys: <Conflicts with TypeScript>
+	if (process.env["HERMES_PRODUCTION"] !== "true") {
+		return src;
+	}
 	if (src.endsWith(".svg")) {
 		return src;
 	}
