6434f468cc0b9a449c2ec5add164d39d2b985c7e
Borna Punda <borna.punda@carbophile.org>
2025-12-23 11:09:02 +0100

    feat(images): specify image sizes (#62)
    All images are 720p in source so this prevents unnecesary scaling and
    bandwidth waste.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
+		deviceSizes: [640, 750, 828, 1080, 1280],
+		imageSizes: [16, 32, 40, 64, 80, 96, 128, 256],
 		loader: "custom",
 		loaderFile: "./lib/image-loader.ts",
 	},
